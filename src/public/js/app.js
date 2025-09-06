f0c39c18fdd89580280166b0e4c5915368a04e80
umsungjun <umseongjun@naver.com>
2025-09-06 15:08:44 +0900

    Feat: 불편한 Websocket 수동 구현 끝

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,4 +1,13 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
 
 const handleOpen = () => {
   console.log("Connected to server ✅");
@@ -9,7 +18,9 @@ const handleClose = () => {
 };
 
 const handleMessage = (message) => {
-  console.log("Message from server: ", message.data);
+  const li = document.createElement("li");
+  li.innerText = `${message.data}`;
+  messageList.appendChild(li);
 };
 
 socket.addEventListener("open", handleOpen);
@@ -17,3 +28,19 @@ socket.addEventListener("open", handleOpen);
 socket.addEventListener("message", handleMessage);
 
 socket.addEventListener("close", handleClose);
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+};
+
+const handleNickSubmit = (event) => {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+};
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -24,8 +24,19 @@ wss.on("connection", (socket) => {
   sockets.push(socket);
   socket.on("close", onSocketClose);
   socket.send("Hello!! from server");
-  socket.on("message", (message) => {
-    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname || "Anonymous"}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
   });
 });
 
